b3ec9da049819b361c923a20d54b54799473401d
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-05 10:23:03 -0400

    componet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//Class componet
+import Text from "./Text"
+const App=()=>{
+  let name="john"
+  return(
+    <div>
+      <center>
+        <h2>Hi This is {name}</h2>
+        <Text/>
+      </center>
+    </div>
+  )
+}
+export default App;
---
+++ src/Text.js
@@ -0,0 +1,15 @@
+// Functional componet
+import react from "react"
+class Text extends react.Component{
+    render(){
+        let name="Dhananjaya"
+        return(
+            <div>
+                <center>
+                    <h2>Hi my name is.{name}</h2>
+                </center>
+            </div>
+        )
+    }
+}
+export default Text;
